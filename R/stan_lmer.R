cc339c54a36598c50dbd8984830c66f4ad6d4b75
jgabry <jgabry@gmail.com>
2015-04-21 10:47:58 -0400

    remove unnecessary files

---
+++ R/stan_lmer.R
@@ -0,0 +1,18 @@
+#' stan_lmer
+#' 
+#' @export
+#' 
+
+stan_lmer <- function(fl , data , family=gaussian , prefix=c("b_","v_") , default_prior="dnorm(0,10)" , matt_trick = TRUE, file_name=NULL, ... ){
+  if(is.null(file_name)){
+    warning("Generated stan code should go into a file with .stan suffix")
+  }
+  glim_result <- glimmer(fl , data , family, prefix=c("b_","v_") , default_prior="dnorm(0,10)" , matt_trick,... )
+  stan_code <- map2stan(glim_result$f, glim_result$d, sample=FALSE)
+  if(!is.null(file_name)){
+    filewrite <- file(file_name)
+    writeLines(stan_code$model, filewrite)
+    close(filewrite)
+  }
+  invisible(list(stan_code = stan_code$model, data=stan_code$data))
+}
