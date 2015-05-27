70fb74dd5404453aa1143a52aeb73b1b5220f656
Rob Trangucci <robert.trangucci@gmail.com>
2015-05-27 14:57:42 -0400

    stan_lmer run_stan = TRUE returns data

--- R/stan_lmer.R
+++ R/stan_lmer.R
@@ -5,7 +5,7 @@
 #' @param family
 #' @param prefix
 #' @param default_prior Default prior for 'fixed effects'.
-#' @param matt_trick Logical. Use non-centered parameterizations?
+#' @param loc_scale_transform Logical. Use non-centered parameterizations?
 #' @param file_name Full path for .stan file to be created.
 #' @param run_stan Logical. Should the model be run in RStan in addition to creating the .stan file?
 #'
@@ -19,7 +19,7 @@ stan_lmer <- function(formula, data, family = gaussian, prefix = c("b_","v_"),
   if(is.null(file_name)){
     warning("Generated stan code should go into a file with .stan suffix")
   }
-  glim_result <- glimmer(formula , data, family, prefix=c("b_","v_") , default_prior="dnorm(0,10)" , loc_scale_transform)
+  glim_result <- glimmer(formula , data, family, prefix=c("b_","v_") , default_prior=default_prior , loc_scale_transform)
   stan_code <- map2stan(glim_result$f, glim_result$d, sample = FALSE)
   if(!is.null(file_name)){
     filewrite <- file(file_name)
@@ -29,7 +29,7 @@ stan_lmer <- function(formula, data, family = gaussian, prefix = c("b_","v_"),
 
   if (run_stan) {
     stanfit <- run_rstan(file = file_name, data = stan_code$data, ...)
-    return(stanfit)
+    return(list(fit = stanfit, data = glim_result$d))
   }
 
   invisible(list(stan_code = stan_code$model, data=stan_code$data))
